3f4d69c5f50e3252516da9d808125cb2611ed229
mdemerat <mdemerat@med.umich.edu>
2016-03-03 12:28:09 -0500

    Changed mockStorage to MockStorage

--- services/src/main/java/edu/umich/hwf/persistence/mockStorage.java
+++ services/src/main/java/edu/umich/hwf/persistence/mockStorage.java
@@ -3,47 +3,44 @@ package edu.umich.hwf.persistence;
 import java.util.HashMap;
 import java.util.Map;
 
-public class mockStorage implements GooglePersistence {
-	HashMap<String, HashMap<String, Integer>> surveyResults = new HashMap<String, HashMap<String, Integer>>();
+public class MockStorage implements GooglePersistence {
+    HashMap<String, HashMap<String, Integer>> surveyResults = new HashMap<String, HashMap<String, Integer>>();
 
-	@Override
-	public boolean persistQuestion(String name, String value) {
+    public boolean persistQuestion(String name, String value) {
 
-		try {
-			if (surveyResults.containsKey(name)) {
+        try {
+            if (surveyResults.containsKey(name)) {
 
-				HashMap<String, Integer> questionValues = surveyResults.get(name);
-				if (questionValues.containsKey(value)) {
-					Integer count = (Integer) questionValues.get(value);
-					questionValues.put(value, count + 1);
-				} else {
-					questionValues.put(value, new Integer(1));
-				}
+                HashMap<String, Integer> questionValues = surveyResults.get(name);
+                if (questionValues.containsKey(value)) {
+                    Integer count = (Integer) questionValues.get(value);
+                    questionValues.put(value, count + 1);
+                } else {
+                    questionValues.put(value, new Integer(1));
+                }
 
-			} else {
-				HashMap<String, Integer> question = new HashMap<String, Integer>();
-				question.put(value, new Integer(1));
-				surveyResults.put(name, question);
-			}
-			return true;
-		} catch (Exception e) {
-			return false;
-		}
-	}
+            } else {
+                HashMap<String, Integer> question = new HashMap<String, Integer>();
+                question.put(value, new Integer(1));
+                surveyResults.put(name, question);
+            }
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
-	@Override
-	public Map<String, Integer> getQuestionResults(String name) {
-		if (surveyResults.containsKey(name)) {
-			return surveyResults.get(name);
-		} else {
-			return null;
-		}
-	}
+    public Map<String, Integer> getQuestionResults(String name) {
+        if (surveyResults.containsKey(name)) {
+            return surveyResults.get(name);
+        } else {
+            return null;
+        }
+    }
 
-	@Override
-	public HashMap<String, HashMap<String, Integer>> getFullResults() {
+    public HashMap<String, HashMap<String, Integer>> getFullResults() {
 
-		return surveyResults;
-	}
+        return surveyResults;
+    }
 
 }
